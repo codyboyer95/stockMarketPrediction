b89dd6ea44bc32a55170fc37ce59f83c0d633acf
Keith Barker <kbarker@ipdanalytics.com>
2022-04-09 22:40:20 -0400

    Update a few metric calculations and add a gitignore for output dirs.

--- RNNanalysis3.py
+++ RNNanalysis3.py
@@ -1,11 +1,14 @@
 
 # source: https://www.youtube.com/watch?v=3eZ56HERVbk
+import os
 import pandas as pd
 import numpy as np
 import matplotlib.pyplot as plt
 import tensorflow as tf
 from keras.models import Sequential
 from keras.layers import Dense, SimpleRNN
+from keras import metrics, losses
+from tensorflow.keras.callbacks import ModelCheckpoint, TensorBoard
 
 
 df = pd.read_csv('HistoricalPrices.csv')
@@ -48,17 +51,41 @@ model = Sequential()
 model.add(SimpleRNN(units=32, input_shape=(1,step), activation='relu'))
 model.add(Dense(8,activation='relu'))
 model.add(Dense(1))
-model.compile(loss='mean_squared_error', optimizer='rmsprop')
+model.compile(loss='huber_loss', metrics=['mean_absolute_error'], optimizer='adam')
 model.summary()
 
-# predict and plot result
-model.fit(trainX, trainY, epochs=10, batch_size=16, verbose=2)
+# create these folders if they does not exist for logging
+if not os.path.isdir("results"):
+    os.mkdir("results")
+    if not os.path.isdir("logs"):
+        os.mkdir("logs")
+
+# Create some callbacks for tracking
+checkpointer = ModelCheckpoint(os.path.join("results", "stockPredictionResults.h5"), save_weights_only=True, save_best_only=True, verbose=1)
+tensorboard = TensorBoard(log_dir=os.path.join("logs", "stockPredictionLog"))
+
+# train the model
+model.fit(
+    trainX,
+    trainY,
+    epochs=50,
+    batch_size=64,
+    validation_data=(testX, testY),
+    callbacks=[checkpointer, tensorboard],
+    verbose=2
+)
+
+# predict the price and plot
 trainPredict = model.predict(trainX)
 testPredict = model.predict(testX)
 predicted=np.concatenate((trainPredict, testPredict), axis=0)
 
 # check the loss
-trainScore = model.evaluate(trainX, trainY, verbose=0)
+trainScore = model.evaluate(testX, testY, verbose=0)
+accuracy = len([x for i, x in enumerate(predicted) if x > df[i]])/len(df)
+print("Total Prediction Accuracy: ", accuracy)
+print("Total Loss: ", trainScore[0])
+print("Total Mean Absolute Error: ", trainScore[1])
 
 days = list(range(len(predicted)))
 vals1 = list(values[step:6000])
